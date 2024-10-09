f3ccff4eac32e5762c4f4420715bbced87fd93a3
renatogomesf <renato_gfe@hotmail.com>
2024-10-09 16:43:36 -0300

    estudo inicial

--- App.tsx
+++ App.tsx
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.box}></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  box: {
+    backgroundColor: "red",
+    width: 100,
+    height: 200,
+  },
+
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
