af4c33f7cabf86da7ed53dd19ebada0763ba164e
renatogomesf <renato_gfe@hotmail.com>
2025-01-15 09:16:53 -0300

    iniciando

---
+++ javascript-básico/script.js
@@ -0,0 +1,10 @@
+
+var nome = prompt("qual seu nome?")
+
+var sobrenome = prompt("qual seu sobrenome?")
+
+console.log(`olá ${nome} ${sobrenome}`);
+
+document.write(`<h1> bem vindo ${nome}</h1>`)
+
+document.write("<img src='https://sujeitoprogramador.com/steve.png' alt='foto' />")
