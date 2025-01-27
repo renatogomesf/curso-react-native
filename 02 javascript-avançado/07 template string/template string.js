0c79a90dc8b86e28be1f79a4e0cbfa67adc38f36
renatogomesf <renato_gfe@hotmail.com>
2025-01-27 11:11:11 -0300

    Create template string.js

---
+++ 02 javascript-avançado/07 template string/template string.js
@@ -0,0 +1,10 @@
+
+// template strings
+
+let nome = 'metheus'
+let sobrenome = 'fraga'
+let idade = 26
+
+let messagem = `meu nome é ${nome} ${sobrenome} e eu tenho ${idade} anos de idade.`
+
+console.log(messagem);
