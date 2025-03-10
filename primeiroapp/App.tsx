67c62aba2149d4ae2828a0a4ec4bf5f16ac33ec4
renatogomesf <renato_gfe@hotmail.com>
2025-03-10 17:16:01 -0300

    modulo básico

--- primeiroapp/App.tsx
+++ primeiroapp/App.tsx
@@ -1,21 +1,38 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
+  let nome = 'Renato';
+  let img = 'https://sujeitoprogramador.com/reactlogo.png';
+
   return (
-    <View style={styles.container}>
-      <Text>Teste de troca de testo</Text>
-      <Text>Renato</Text>
-      <StatusBar style="auto" />
+    <View style={style.section}>
+      <Text style={{ color: 'blue', fontWeight: 'bold' }}>Olá Mundo!</Text>
+      <Text style={style.text}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi ullam
+        harum repellendus sunt ad commodi sint ex? Animi facere facilis maiores
+        pariatur, perferendis id esse, at placeat veniam ad hic.
+      </Text>
+
+      <Image
+        source={{ uri: 'https://sujeitoprogramador.com/reactlogo.png' }}
+        style={{ width: 100, height: 100 }}
+      />
+
+      <Image source={{ uri: img }} style={{ width: 100, height: 100 }} />
+
+      <Text style={{ fontWeight: 'bold' }}>{nome}</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
+const style = StyleSheet.create({
+  section: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    padding: 30,
+    fontSize: 20,
   },
 });
