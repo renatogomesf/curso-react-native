c2521489787da6aced274f2159ccd21a32e7fa79
renatogomesf <renato_gfe@hotmail.com>
2025-03-11 10:01:13 -0300

    aula 06

--- primeiroapp/modul06 basico/aula 05/States.tsx
+++ primeiroapp/modul06 basico/aula 05/States.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-import { Text, View, Button } from 'react-native';
+import { Text, View, Button, StyleSheet } from 'react-native';
 
 export default function States() {
   const [nome, setNome] = useState('');
@@ -13,10 +13,30 @@ export default function States() {
   }
 
   return (
-    <View>
-      <Text style={{ marginBottom: 20 }}>{nome}</Text>
-      <Text style={{ marginBottom: 20 }}>{idade}</Text>
+    <View style={styles.area}>
+      <Text style={[styles.titulo, styles.textoPreto]}>{nome}</Text>
+      <Text style={styles.titulo}>{idade}</Text>
+      <Text style={styles.subTitulo}>Texto qualquer</Text>
       <Button title="Mudar nome" onPress={() => entrar('lucas oliveira', 33)} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  area: {
+    padding: 10,
+    borderWidth: 3,
+    borderColor: 'green',
+  },
+  titulo: {
+    fontSize: 20,
+    color: '#ff0000',
+  },
+  subTitulo: {
+    color: '#00ff00',
+    fontSize: 17,
+  },
+  textoPreto: {
+    color: '#000',
+  },
+});
